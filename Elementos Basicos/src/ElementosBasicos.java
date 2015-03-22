d6b0e43b6c194d4052cbfda5a9f8048df1a6afed
Gabriel <Gabriel@Gabriel-PC>
2015-03-22 13:07:27 +0100

    Elementos Basicos

---
+++ Elementos Basicos/src/ElementosBasicos.java
@@ -0,0 +1,16 @@
+
+public class ElementosBasicos {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=10;
+		int suma;
+		
+			{
+				suma=a+b;
+				System.out.println(suma);
+			}
+
+	}
+
+}
